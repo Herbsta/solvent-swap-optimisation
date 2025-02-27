6256c938bd81ac72d91cf421495e3c4c3ec88628
Herbsta <55990383+Herbsta@users.noreply.github.com>
2025-02-27 16:08:03 +0000

    Update Water_Select.sql

--- src/2-featureEngineering/Water_Select.sql
+++ src/2-featureEngineering/Water_Select.sql
@@ -1,19 +1,21 @@
 -- Select statement for all permuatations that contain H20 as a solvent
 SELECT 
     compound_id AS api,
-    solubility_g_g as solubility,
     CASE 
         WHEN solvent_1 = 962 THEN solvent_2
         ELSE solvent_1
     END AS solvent,
+    ROUND(solubility_g_g,4) as solubility,
     CASE 
-        WHEN solvent_1 = 962 THEN 1 - solvent_1_weight_fraction
-        ELSE solvent_1_weight_fraction
+        WHEN solvent_1 = 962 THEN 1 - ROUND(solvent_1_weight_fraction,2)
+        ELSE ROUND(solvent_1_weight_fraction,2)
     END AS fraction,
-    temperature
+    ROUND(temperature) as temperature 
 FROM
     solubility s
 WHERE 
     (solvent_2 = 962 OR solvent_1 = 962)
     AND solvent_2 IS NOT NULL
-    AND (solvent_1_weight_fraction <> 1);
+    AND solubility <> 0
+    AND fraction <> 1
+	AND fraction <> 0;
